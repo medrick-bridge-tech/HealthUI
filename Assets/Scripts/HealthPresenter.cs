0b6384648c4951440b412aaadfbe70793d0ba6ee
Codidodido <codidodido@gmail.com>
2023-09-25 17:05:40 +0330

    Create health presenter script

---
+++ Assets/Scripts/HealthPresenter.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPresenter : MonoBehaviour
+{
+    [SerializeField] private Health _health;
+    [SerializeField] private Image _healthBarFill;
+
+    
+    private void OnEnable()
+    {
+        if (_health.onHealthChanged != null)
+        {
+            _health.onHealthChanged += UpdateHealthBar;
+        }
+    }
+    
+    private void OnDisable()
+    {
+        if (_health.onHealthChanged != null)
+        {
+            _health.onHealthChanged -= UpdateHealthBar;
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        _healthBarFill.fillAmount = _health.CurrentHealth / _health.MaxHealth;
+    }
+}
